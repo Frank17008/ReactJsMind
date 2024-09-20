f32edf73181d37d4ba3946cb3f5d03d8aab78774
Frank17008 <405267330@qq.com>
2024-09-20 17:23:29 +0800

    fix: demo修改

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { NodeArrayData, NodeTreeData } from './mock'
 // import ReactJsMind from '../dist/index'
@@ -7,7 +7,7 @@ import 'react-jsmind/dist/index.min.css'
 import './index.less'
 
 const App = () => {
-  const mindRef: any = useState(null)
+  const mindRef: any = useRef(null)
   const [data, setData] = useState(NodeTreeData)
   const [editable, setEditable] = useState(true)
   const getData = () => {
@@ -25,6 +25,9 @@ const App = () => {
   const onNodeClick = (node) => {
     console.log('点击的节点', node)
   }
+  const onExpand = (node) => {
+    console.log(`节点${node.expanded ? "展开" : "收起"}`, node)
+  }
   return (
     <div style={{ width: '100%', height: 400 }}>
       <div className='btns'>
@@ -32,7 +35,7 @@ const App = () => {
         <button onClick={changeData}>更换数据</button>
         <button onClick={enableEdit}>{editable ? '关闭' : '开启'}编辑</button>
       </div>
-      <ReactJsMind ref={mindRef} options={{ editable }} data={data} onClick={onNodeClick} />
+      <ReactJsMind ref={mindRef} options={{ editable }} data={data} onClick={onNodeClick} onExpand={onExpand} />
     </div>
   )
 }
